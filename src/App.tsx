ab86f6f54de7eed168cb1c7a6e9dae73a8e4c603
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-10-28 12:18:13 +0545

    base set

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import "./App.css";
+
 function App() {
   return (
-    <>
-      <h1 className='text-4xl'>Hello, world!</h1>
-    </>
-  )
+    <div className="App">
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/About.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2 className="text-2xl">About Me</h2>
+      <p>
+        Hi, I am a Frontend Web Developer passionate about creating stunning
+        user interfaces.
+      </p>
+    </section>
+  );
+};
+
+export default About;
